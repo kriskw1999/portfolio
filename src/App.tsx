c0dba6fb22f57190bf8a1851dc02592b1b8e3905
krzysztofwitkowski <krzysztof.witkowski@sysconsgroup.com>
2023-12-30 11:00:12 +0100

    feat(passed to fiber): passed my part of the project to fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import "./style/main.scss";
-import MainSection from "./components/MainSection.tsx";
-import React, { Suspense } from "react";
+import './style/main.scss'
+import MainSection from './components/MainSection.tsx'
+import React, { Suspense } from 'react'
 
-const PersonalInfo = React.lazy(() => import("./components/PersonalInfo.tsx"));
-const SkillsSection = React.lazy(
-  () => import("./components/SkillsSection.tsx")
-);
+const PersonalInfo = React.lazy(() => import('./components/PersonalInfo.tsx'))
+const SkillsSection = React.lazy(() => import('./components/SkillsSection.tsx'))
 
 function App() {
   return (
     <div className="container">
       <MainSection />
+
       <Suspense>
         <SkillsSection />
       </Suspense>
+
       <Suspense>
         <PersonalInfo />
       </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -1,89 +1,45 @@
-import { useEffect, useRef } from "react";
-import * as THREE from "three";
-import g from "../assets/github_shape.svg";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera, useTexture } from '@react-three/drei'
+import g from '../assets/github_shape.svg'
+import { Mesh } from 'three'
 
-function Sphere() {
-  const containerRef = useRef<HTMLDivElement>(null);
+function SphereMesh() {
+  const texture = useTexture(g)
+  const sphereRef = useRef<Mesh | null>(null)
 
-  useEffect(() => {
-    if (containerRef.current && containerRef.current.children.length === 0) {
-      // Set up the scene
-      const scene = new THREE.Scene();
-      const canvasSize = containerRef.current.offsetWidth;
+  useFrame(() => {
+    if (!sphereRef.current) return
 
-      // Set up the camera
-      const camera = new THREE.PerspectiveCamera(
-        75,
-        containerRef.current.clientWidth / containerRef.current.clientHeight,
-        0.1,
-        1000
-      );
-      camera.position.z = 15;
+    sphereRef.current.rotation.y += 0.02
+  })
 
-      // Set up the renderer
-      const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-      });
-      renderer.setSize(
-        containerRef.current.clientWidth,
-        containerRef.current.clientHeight
-      );
-
-      // Add ambient light to the scene
-      const pointLight = new THREE.PointLight(0xffffff);
-      const globalLight = new THREE.AmbientLight(0xffffff, 0.5);
-      pointLight.position.set(100, 100, 100);
-      scene.add(pointLight, globalLight);
-
-      const controls = new OrbitControls(camera, renderer.domElement);
-      controls.maxDistance = 40;
-      controls.minDistance = 12;
-      controls.enableRotate = false;
-
-      // const lightHelper = new THREE.PointLightHelper(pointLight);
-      // scene.add(lightHelper);
-
-      containerRef.current.appendChild(renderer.domElement);
-
-      // Create a sphere geometry
-      const geometry = new THREE.SphereGeometry(
-        (canvasSize + 500) / 200,
-        64,
-        32
-      );
-
-      // Create a basic material and assign it to the sphere
-      // Load the SVG image as a texture
-      const textureLoader = new THREE.TextureLoader();
-      const texture = textureLoader.load(g);
-
-      const material = new THREE.MeshStandardMaterial({
-        map: texture,
-        roughness: 0.5,
-        fog: true,
-      });
-      const sphere = new THREE.Mesh(geometry, material);
-
-      // Add the sphere to the scene
-      scene.add(sphere);
-
-      // Animation loop
-      const animate = () => {
-        requestAnimationFrame(animate);
-        controls.update();
-        sphere.rotation.y += 0.02;
-        renderer.render(scene, camera);
-      };
-
-      animate();
-    }
-  }, []);
-
-  return <div ref={containerRef} className="canvas" />;
+  return (
+    <mesh rotation={[0, 0, 0]} ref={sphereRef}>
+      <sphereGeometry args={[2.5, 64, 32]} />
+      <meshStandardMaterial map={texture} roughness={0.5} fog />
+    </mesh>
+  )
 }
 
-export default Sphere;
+function Sphere() {
+  return (
+    <div className="canvas">
+      <Canvas>
+        <Suspense fallback={null}>
+          <PerspectiveCamera position={[0, 0, 15]} />
+          <OrbitControls
+            maxDistance={40}
+            minDistance={6}
+            enableRotate={false}
+          />
+          <ambientLight intensity={0.5} />
+          <pointLight intensity={5000} position={[100, 100, 100]} />
+          <SphereMesh />
+        </Suspense>
+      </Canvas>
+    </div>
+  )
+}
+
+export default Sphere
